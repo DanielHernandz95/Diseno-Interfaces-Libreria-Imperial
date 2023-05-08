fbcc027d8f3600f851e09635e0f214c08e92faf6
Daniel Mateo Hernandez Acevedo <dm.hernandeza@uniandes.edu.co>
2023-05-07 19:39:06 -0500

    subida con carrusel

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Navmenu from './components/menu/Navmenu';
 import Footerpage from './components/footer/Footerpage';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <div><Navmenu /></div>
+      <div><Home /></div>
       <div className='footerpage'><Footerpage /></div>
     </div>
   );
--- src/components/menu/Navmenu.js
+++ src/components/menu/Navmenu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Navmenu.css';
+import logoimperial from './logo-imperial.png';
 
 
 
@@ -8,14 +9,38 @@ const Navmenu = () => {
     return (
         <div className='menus'>
             <div className='logo'>
-                <p>logo</p>
+            <img src={logoimperial} alt="logoimperial" className='logoimperial' id="logoimperial" />
             </div>
             <div className='menubars'>
                 <div className='menutop'>
-                    <p>menutop</p>
+                    <nav className="navigatortop">
+                        <ul>
+                            <li>Ayuda</li>
+                            <li>Contacto</li>
+                            <li>Registrarse</li>                            
+                        </ul>
+                    </nav>
                 </div>
-                <div className='menubottom'>
-                    <p>menubottom</p>
+                <div className='menubottom'>                    
+                    <nav className="navigatorbottom">
+                        <div>
+                            <ul>
+                                <li>Novedades</li>
+                                <li>Comics</li>
+                                <li>Catálogo</li>
+                                <li className="itemmarvel">Marvel</li>
+                                <li>Figuras de acción</li>
+                            </ul>
+                        </div>
+                        <div className="iconosnav">
+                            <ul>
+                                <li>lupa</li>
+                                <li>carrito</li>
+                                <li>user</li>                            
+                            </ul>
+                        </div>
+                        
+                    </nav>
                 </div>
             </div>
             
