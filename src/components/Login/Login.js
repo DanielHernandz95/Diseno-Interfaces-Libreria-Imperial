8edd72980b28f433ffd0eb2fe0ebc2c5dd20449e
Lorena Peraza <diegotri.89@gmail.com>
2023-05-17 10:03:05 -0500

    L

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,83 @@
+import React, { useState } from 'react';
 import './Login.css';
 import Navmenu from '../menu/Navmenu';
 import Footerpage from '../footer/Footerpage';
 
 export default function Login() {
-    
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (username.trim() === '') {
+            setError('Por favor, ingresa tu nombre de usuario');
+            return;
+        }
+
+        if (password.trim() === '') {
+            setError('Por favor, ingresa tu contraseña');
+            return;
+        }
+
+        if (username === 'DiseñoInterfaces' && password === 'd1s3ñ0123') {
+            console.log('Inicio de sesión exitoso');
+            window.location.href = '/Home';
+        } else {
+            setError('Nombre de usuario o contraseña incorrectos');
+        }
+
+        setUsername('');
+        setPassword('');
+    };
+
     return (
         <>
-        <div className='App'>
-            <div><Navmenu /></div>
-            <div className="login">
-                <h1> INICIAR SESION </h1>
-            </div>
-            <div className='footerpage'><Footerpage /></div>
-            </div>
+            <div className="App">
+                <div>
+                    <Navmenu />
+                </div>
+                
+                    <h1>INICIAR SESIÓN</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="username">Usuario:</label>
+                            <input
+                                type="text"
+                                id="username"
+                                value={username}
+                                onChange={handleUsernameChange}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Contraseña:</label>
+                            <input
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={handlePasswordChange}
+                            />
+                        </div>
+                        {error && <p className="error">{error}</p>}
+                        <div className="center">
+                            <button type="submit">Ingresar</button>
+                        </div>
+                    </form>
+            
+                </div>
+                <div className="footerpage">
+                    <Footerpage />
+                </div>
+            
         </>
-    )
-  }
+    );
+}
--- src/components/Registro/Registro.js
+++ src/components/Registro/Registro.js
@@ -1,18 +1,115 @@
 import './Registro.css';
 import Navmenu from '../menu/Navmenu';
 import Footerpage from '../footer/Footerpage';
+import { useState } from 'react';
+import BannerHome from '../Home/Banner';
 
 export default function Registro() {
+    const [nombres, setNombres] = useState('');
+    const [apellidos, setApellidos] = useState('');
+    const [email, setEmail] = useState('');
+    const [fechaNacimiento, setFechaNacimiento] = useState('');
+    const [contrasena, setContrasena] = useState('');
+    const [aceptaTerminos, setAceptaTerminos] = useState(false);
+
+    const handleInputChange =(e) => {
+        const { name, value } = e.traget;
+
+        if (name === 'nombres') {
+            setNombres(value);
+          } else if (name === 'apellidos') {
+            setApellidos(value);
+          } else if (name === 'email') {
+            setEmail(value);
+          } else if (name === 'fechaNacimiento') {
+            setFechaNacimiento(value);
+          } else if (name === 'contrasena') {
+            setContrasena(value);
+          }
+        };
+
+        const handleCheckboxChange = (e) => {
+            setAceptaTerminos(e.target.checked);
+          };
+        
+          const handleSubmit = (e) => {
+            e.preventDefault();
+
+          };
     
     return (
         <>
         <div className='App'>
             <div><Navmenu /></div>
             <div className="registro">
-                <h1> REGISTRO </h1>
+                <h1> REGISTRARSE </h1>
+                <form onSubmit={handleSubmit}>
+                    <lavel htmlFor="nombres">Nombres: </lavel>
+                    <input
+                        type="text"
+                        id="nombres"
+                        value={nombres}
+                        onChange={(e) => setNombres(e.traget.value)}
+                        required
+                    />
+
+                    <lavel htmlFor="apellidos">Apellidos: </lavel>
+                    <input
+                        type="text"
+                        id="apellidos"
+                        value={apellidos}
+                        onChange={(e) => setApellidos(e.traget.value)}
+                        required
+                    />
+
+                    <lavel htmlFor="email">Email: </lavel>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.traget.value)}
+                        required
+                    />
+
+                    <lavel htmlFor="fechaNacimiento">Fecha de Nacimiento: </lavel>
+                    <input
+                        type="date"
+                        id="fechaNacimiento"
+                        value={fechaNacimiento}
+                        onChange={(e) => setFechaNacimiento(e.traget.value)}
+                        required
+                    />
+                    
+                    <lavel htmlFor="contrasena">Contraseña: </lavel>
+                    <input
+                        type="password"
+                        id="contrasena"
+                        value={contrasena}
+                        onChange={(e) => setContrasena(e.traget.value)}
+                        required
+                    />
+
+                    <input
+                        type="checkbox"
+                        id="aceptaTerminos"
+                        checked={aceptaTerminos}
+                        onChange={(e) => setAceptaTerminos(e.traget.value)}
+                        required
+                    />
+                    <lavel htmlFor="aceptaTerminos">
+                        Acepto los terminos de uso y politica de privacidad
+                    </lavel>
+
+                    <div className="center">
+                        <button type="submit">Crear</button>
+                    </div>
+
+                </form>
             </div>
-            <div className='footerpage'><Footerpage /></div>
+
+                <div className='footerpage'><Footerpage /></div>
+                
             </div>
         </>
-    )
+    );
   }
